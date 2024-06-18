caa472b16166c530f8d781e38b4ab9f7900c307e
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-06-18 17:10:56 +0200

    Posrgresql

--- my_shop/my_shop/settings.py
+++ my_shop/my_shop/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = "my_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 DATABASES = {
